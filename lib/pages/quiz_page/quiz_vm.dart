2563e3867a1ae68f780f382d1978909779fdcb3a
BryanLiang <liangrui.ch@gmail.com>
2024-12-30 20:54:46 +0800

    完善QuizViewModel

--- lib/pages/quiz_page/quiz_vm.dart
+++ lib/pages/quiz_page/quiz_vm.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:rubbish_detection/repository/data/quiz.dart';
@@ -8,16 +10,20 @@ class QuizViewModel with ChangeNotifier {
   Future<void> getQuiz() async {
     final dio = Dio(BaseOptions(baseUrl: "http://10.133.73.147:1760"));
 
-    final res = await dio.get("/api/quiz/random");
+    try {
+      final res = await dio.get("/api/quiz/random");
 
-    final model = QuizModel.fromJson(res.data);
+      final model = QuizModel.fromJson(res.data);
 
-    if (quizList.isNotEmpty) {
-      quizList.clear();
+      if (quizList.isNotEmpty) {
+        quizList.clear();
+      }
+
+      quizList.addAll(model.data ?? []);
+    } catch (e) {
+      log("Error fetching quiz: $e");
+    } finally {
+      notifyListeners();
     }
-
-    quizList.addAll(model.data ?? []);
-
-    notifyListeners();
   }
 }
