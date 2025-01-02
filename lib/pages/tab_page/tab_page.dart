dfaa230e5f16bfb65e5a6a1e7746b009b7c92e2a
BryanLiang <liangrui.ch@gmail.com>
2025-01-03 00:28:55 +0800

    改用lazy_load_indexed_stack实现tab懒加载

--- lib/pages/tab_page/tab_page.dart
+++ lib/pages/tab_page/tab_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lazy_load_indexed_stack/lazy_load_indexed_stack.dart';
 import 'package:rubbish_detection/pages/discovery_page/discovery_page.dart';
 import 'package:rubbish_detection/pages/home_page/home_page.dart';
 import 'package:rubbish_detection/pages/personal_page/personal_page.dart';
@@ -12,85 +13,79 @@ class TabPage extends StatefulWidget {
 }
 
 class _TabPageState extends State<TabPage> {
-  int currentIdx = 0;
+  final _currentIndexNotifier = ValueNotifier<int>(0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: IndexedStack(
-          index: currentIdx,
-          children: const [
-            HomePage(),
-            DiscoveryPage(),
-            PersonalPage(),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          selectedLabelStyle: TextStyle(fontSize: 14.sp), // 选中的样式
-          unselectedLabelStyle: TextStyle(fontSize: 12.sp), // 未选中的样式
-          currentIndex: currentIdx,
-          items: _barItemList(),
-          // 点击切换页面
-          // index是children中元素的下标
-          onTap: (index) {
-            setState(() {
-              currentIdx = index;
-            });
-          },
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: _currentIndexNotifier,
+      builder: (context, currentIndex, child) {
+        return Scaffold(
+          body: SafeArea(
+            child: LazyLoadIndexedStack(
+              index: currentIndex,
+              children: const [
+                HomePage(),
+                DiscoveryPage(),
+                PersonalPage(),
+              ],
+            ),
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              selectedLabelStyle: TextStyle(fontSize: 14.sp), // 选中的样式
+              unselectedLabelStyle: TextStyle(fontSize: 12.sp), // 未选中的样式
+              currentIndex: currentIndex,
+              items: _buildBarList(),
+              // 点击切换页面
+              // index是children中元素的下标
+              onTap: (index) => _currentIndexNotifier.value = index,
+            ),
+          ),
+        );
+      },
     );
   }
 
-  List<BottomNavigationBarItem> _barItemList() {
+  List<BottomNavigationBarItem> _buildBarList() {
     return [
-      BottomNavigationBarItem(
-        label: "首页",
-        activeIcon: Image.asset(
-          "assets/images/home_icon_selected.png",
-          width: 25.r,
-          height: 25.r,
-        ),
-        icon: Image.asset(
-          "assets/images/home_icon_unselect.png",
-          width: 25.r,
-          height: 25.r,
-        ),
+      _buildBarItem(
+        "首页",
+        "assets/images/home_icon_unselect.png",
+        "assets/images/home_icon_selected.png",
       ),
-      BottomNavigationBarItem(
-        label: "发现",
-        activeIcon: Image.asset(
-          "assets/images/discovery_icon_selected.png",
-          width: 25.r,
-          height: 25.r,
-        ),
-        icon: Image.asset(
-          "assets/images/discovery_icon_unselect.png",
-          width: 25.r,
-          height: 25.r,
-        ),
+      _buildBarItem(
+        "发现",
+        "assets/images/discovery_icon_unselect.png",
+        "assets/images/discovery_icon_selected.png",
       ),
-      BottomNavigationBarItem(
-        label: "我的",
-        activeIcon: Image.asset(
-          "assets/images/personal_icon_selected.png",
-          width: 25.r,
-          height: 25.r,
-        ),
-        icon: Image.asset(
-          "assets/images/personal_icon_unselect.png",
-          width: 25.r,
-          height: 25.r,
-        ),
-      )
+      _buildBarItem(
+        "我的",
+        "assets/images/personal_icon_unselect.png",
+        "assets/images/personal_icon_selected.png",
+      ),
     ];
+  }
+
+  BottomNavigationBarItem _buildBarItem(
+      String label, String iconPath, String activeIconPath) {
+    return BottomNavigationBarItem(
+      label: label,
+      activeIcon: Image.asset(
+        activeIconPath,
+        width: 25.r,
+        height: 25.r,
+      ),
+      icon: Image.asset(
+        iconPath,
+        width: 25.r,
+        height: 25.r,
+      ),
+    );
   }
 }
