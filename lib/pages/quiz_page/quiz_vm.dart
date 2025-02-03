75b4dfd47cff47ae3e8a7830e2e1217231f81e86
BryanLiang <liangrui.ch@gmail.com>
2025-02-04 02:44:55 +0800

    完善知识小测和后端的对接

--- lib/pages/quiz_page/quiz_vm.dart
+++ lib/pages/quiz_page/quiz_vm.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:rubbish_detection/http/dio_instance.dart';
 import 'package:rubbish_detection/repository/data/quiz.dart';
 
 class QuizViewModel with ChangeNotifier {
@@ -12,12 +12,10 @@ class QuizViewModel with ChangeNotifier {
     isLoading = true;
     notifyListeners();
 
-    final dio = Dio(BaseOptions(baseUrl: "http://10.133.73.147:1760"));
-
     try {
       quizList.clear();
 
-      final res = await dio.get("/api/quiz/random");
+      final res = await DioInstance.instance.get("/api/quiz/random");
 
       final model = QuizModel.fromJson(res.data);
 
